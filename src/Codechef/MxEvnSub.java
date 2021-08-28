bb58134d615f2560fd545969399822e0a8272b5d
adity <adityapratap620@gmail.com>
2021-08-29 00:04:52 +0530

    Codechef

--- src/Codechef/MxEvnSub.java
+++ src/Codechef/MxEvnSub.java
@@ -6,7 +6,6 @@ import java.lang.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 public class MxEvnSub {
-
     public static void main (String[] args) throws java.lang.Exception
     {
         // your code goes here
