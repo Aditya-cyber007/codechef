b1901a1a03144ded505ecfa30e595aefcdac9385
adity <adityapratap620@gmail.com>
2021-09-05 15:28:10 +0530

    Codechef September long challenge Q1

---
+++ src/Codechef/AirlineRestriction.java
@@ -0,0 +1,30 @@
+package Codechef;
+
+import java.util.Scanner;
+
+public class AirlineRestriction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-->0){
+            int A,B,C,D,E;
+            A = sc.nextInt();
+            B = sc.nextInt();
+            C = sc.nextInt();
+            D = sc.nextInt();
+            E = sc.nextInt();
+            if ((A+B)<= D && C <= E) {
+                System.out.println("YES");
+            }
+            else if ((B+C)<= D && A <= E) {
+                System.out.println("YES");
+            }
+            else if ((C+A)<= D && B <= E) {
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
