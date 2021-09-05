be4db283c390c54a341f41cccaca991f7c6069da
adity <adityapratap620@gmail.com>
2021-09-05 17:16:21 +0530

    Codechef September long challenge Q2

---
+++ src/Codechef/TravelPass.java
@@ -0,0 +1,32 @@
+package Codechef;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.lang.String;
+
+public class TravelPass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-->0){
+            int N = sc.nextInt();
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            sc.nextLine();
+            String S = sc.nextLine();
+            int count1=0 , count0 = 0;
+            char[] ch = S.toCharArray();
+            for (int i = 0; i < N; i++) {
+                char a=ch[i];
+                if (a=='1'){
+                    count1++;
+                }
+                else if (a=='0'){
+                    count0++;
+                }
+
+            }
+            System.out.println((count0*A) + (count1*B));
+        }
+    }
+}
