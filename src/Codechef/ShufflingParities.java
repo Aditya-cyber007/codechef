bd27e75aecdee1262fd1e00f02c3238191e6f373
ADITYA PRATAP SINGH <57958826+adityapratap007@users.noreply.github.com>
2021-09-10 12:59:48 +0530

    Update ShufflingParities.java

--- src/Codechef/ShufflingParities.java
+++ src/Codechef/ShufflingParities.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ShufflingParities {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         while (T-->0){
